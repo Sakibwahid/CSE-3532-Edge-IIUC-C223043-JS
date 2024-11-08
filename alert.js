fca7582b692d6373ac490be82fc353c0aa33668a
Nurul Azim Sakib <sakibruns@gmail.com>
2024-11-08 12:46:32 +0600

    class commit

---
+++ alert.js
@@ -0,0 +1,29 @@
+"use strict"
+// alert('internal js');
+// alert('internal line 2');
+// alert(1+
+//     2
+//     +3
+// );
+
+// // alert("Hello");
+// // alert( NaN + 1 ); // NaN
+// // alert( 3 * NaN ); // NaN
+// // alert( "not a number" / 2 - 1 )
+
+
+// [1, 2].forEach(alert);
+let username;
+
+// alert( `hello ${1}` ); 
+
+// alert( `hello ${"name"}` ); 
+
+// alert( `hello ${username}` ); 
+// let age=prompt('how old are you?')
+let a = Number(prompt("First number?", 1));
+let b = Number(prompt("Second number?", 2));
+// a=Number(a);
+// b=Number(b);
+
+alert(a + b); 
