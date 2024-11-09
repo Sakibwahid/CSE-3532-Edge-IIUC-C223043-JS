4b67e9db7d0b2a9cd14ff7547ec6a855907a3b68
Nurul Azim Sakib <sakibruns@gmail.com>
2024-11-09 16:22:12 +0600

    reviewed the properties in home

--- alert.js
+++ alert.js
@@ -21,9 +21,20 @@ let username;
 
 // alert( `hello ${username}` ); 
 // let age=prompt('how old are you?')
-let a = Number(prompt("First number?", 1));
+let a = Number(prompt("First number?", 67));
 let b = Number(prompt("Second number?", 2));
 // a=Number(a);
 // b=Number(b);
 
-alert(a + b); 
+console.log(a + b); 
+
+let name = "Ilya";
+
+// the expression is a number 1
+alert( `hello ${'1'}` ); // hello 1
+
+// the expression is a string "name"
+alert( `hello ${"name"}` ); // hello name
+
+// the expression is a variable, embed it
+alert( `${name}` ); // hello Ilya
