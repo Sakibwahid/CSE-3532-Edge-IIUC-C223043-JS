09e771e9524ceb5fccd10d0741565db1a57a63b5
Nurul Azim Sakib <sakibruns@gmail.com>
2024-11-12 01:47:58 +0600

    string embed

--- alert.js
+++ alert.js
@@ -13,7 +13,7 @@
 
 
 // [1, 2].forEach(alert);
-let username;
+// let username;
 
 // alert( `hello ${1}` ); 
 
@@ -21,20 +21,14 @@ let username;
 
 // alert( `hello ${username}` ); 
 // let age=prompt('how old are you?')
-let a = Number(prompt("First number?", 67));
-let b = Number(prompt("Second number?", 2));
+// let a = Number(prompt("First number?", 67));
+// let b = Number(prompt("Second number?", 2));
 // a=Number(a);
 // b=Number(b);
 
-console.log(a + b); 
+// console.log(a + b); 
 
-let name = "Ilya";
+// let temp= true;
+// alert(typeoftemp);
+// alert(typeof(String(temp)))
 
-// the expression is a number 1
-alert( `hello ${'1'}` ); // hello 1
-
-// the expression is a string "name"
-alert( `hello ${"name"}` ); // hello name
-
-// the expression is a variable, embed it
-alert( `${name}` ); // hello Ilya
