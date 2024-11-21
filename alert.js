a073e22ad93a3d3dc7a2d70c8c0f60448b53a535
Nurul Azim Sakib <sakibruns@gmail.com>
2024-11-22 02:44:42 +0600

    Dom

--- alert.js
+++ alert.js
@@ -29,6 +29,23 @@
 // console.log(a + b); 
 
 // let temp= true;
-// alert(typeoftemp);
+// alert(typeof(temp));
 // alert(typeof(String(temp)))
 
+let loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("click", verifyLogin);
+
+let emailInput = document.getElementById("email");
+let passwordInput = document.getElementById("password");
+
+function verifyLogin() {
+    let emailValue = emailInput.value;
+    let passwordValaue = passwordInput.value;
+    if(emailValue === 'secret@gmail.com' && passwordValaue === 'secret') {
+        alert('Dashboard');
+        // redirect to dashboard
+        window.location.replace("dashboard.html");
+    } else {
+        alert('You are not authorized to enter');
+    }
+}
